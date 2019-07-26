a1a03b0186ce44fc5dbb2db3ee47910f88fba04b
nchamo <nicolas@chamo.com.ar>
2019-07-26 11:01:53 -0300

    Fix docker build

--- app/models/task.py
+++ app/models/task.py
@@ -358,7 +358,7 @@ class Task(models.Model):
                 files = [{'fileName': image['file'], 'fileUrl': image['element_url']} for image in result['images']]
                 logger.info("Found the following images to download {}".format(files))
                 
-                # We are keeping images and txt files, and we are mapping the txt file to the appropriate name
+                # We are keeping images and gcp files, and we are mapping the gcp files to the appropriate name
                 files = [self.map_gcp_file_if_necessary(file) for file in files if self.is_valid_file(file)]
 
                 if len(result['images']) != len(files):
@@ -403,14 +403,15 @@ class Task(models.Model):
 
     def map_gcp_file_if_necessary(self, file):
         _, file_extension = os.path.splitext(file['fileName'])
-        if file_extension.lower() == ".txt" and 'gcp' in file['fileName']:
+        if file_extension.lower() == ".txt" and 'gcp_list' in file['fileName']:
             file['fileName'] = 'gcp_list.txt'
         return file
 
     def is_valid_file(self, file):
-        VALID_EXTENSIONS = ['.tiff', '.tif', '.bmp', '.png', '.jpeg', '.jpg', '.gif', '.svg', '.txt']
+        VALID_IMAGE_EXTENSIONS = ['.tiff', '.tif', '.bmp', '.png', '.jpeg', '.jpg', '.gif', '.svg']
         _, file_extension = os.path.splitext(file['fileName'])
-        return file_extension.lower() in VALID_EXTENSIONS
+        return (file_extension.lower() in VALID_IMAGE_EXTENSIONS) or
+            (file_extension.lower() == '.txt' and 'gcp_list' in file['fileName'])
 
     def handle_import(self):
         self.console_output += "Importing assets...\n"
